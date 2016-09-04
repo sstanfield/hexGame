396d271009b237029174655db32554b22822770d
Steven Stanfield <stanfield@scarecrowtech.com>
2016-09-04 17:43:28 -0400

    Small twekas for OSX compiles.

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ freely, subject to the following restrictions:
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 #include <signal.h>
 #include <thread>
 #include <chrono>
